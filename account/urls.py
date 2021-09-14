97f09d0d05e07ef0b973584d1090690ea26bbeec
Jefferson <jeffersonximenes1@gmail.com>
2021-09-14 20:47:51 -0300

    url para a view edit

--- account/urls.py
+++ account/urls.py
@@ -1,3 +1,4 @@
+from os import name
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
@@ -15,4 +16,5 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('', include('django.contrib.auth.urls')),
     path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
 ]
