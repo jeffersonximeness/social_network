5d700209c1b10190ecd3c8a2db8becab323da21c
Jefferson <jeffersonximenes1@gmail.com>
2021-09-14 20:22:23 -0300

    corrigindo bug

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -127,5 +127,5 @@ LOGOUT_URL = 'logout'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-MEDIA_URL = '/media'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
