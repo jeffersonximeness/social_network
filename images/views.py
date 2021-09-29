a9968d7b6134e68872b3d19e14e06f4c97230bd4
jeffersonximeness <jeffersonximenes1@gmail.com>
2021-09-29 20:19:26 -0300

    Decorador para as views

--- images/views.py
+++ images/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 
+from bookmarks.common.decorators import ajax_required
 from .forms import ImageCreateForm
 from .models import Image
 
@@ -32,6 +33,7 @@ def image_detail(request, id, slug):
 
     return render(request, 'images/image/detail.html', {'section': 'images', 'image': image})
 
+@ajax_required
 @login_required
 @require_POST
 def image_like(request):
