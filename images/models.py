fd9e1678c124190f1e37eaee8dd9387e6a53e129
jeffersonximeness <jeffersonximenes1@gmail.com>
2021-09-29 17:55:57 -0300

    View de detalhes da imagem e transformacao da imagem em thumbnail

--- images/models.py
+++ images/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
+from django.urls import reverse
 
 
 class Image(models.Model):
@@ -15,6 +16,9 @@ class Image(models.Model):
     description = models.TextField(blank=True)
     created = models.DateField(auto_now_add=True, db_index=True)
     users_like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='images_liked', blank=True)
+
+    def get_absolute_url(self):
+        return reverse('images:detail', args=[self.id, self.slug])
 
 
     def __str__(self):
