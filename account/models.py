b12fa698e5511102e2a08cd52f35dfbac3a597e1
jeffersonximeness <jeffersonximenes1@gmail.com>
2021-10-05 15:35:04 -0300

    Sistema de seguidores. Relacionamento de muitos para muitos com modelo intermediário

--- account/models.py
+++ account/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.conf import settings
+from django.db.models.fields import related
+from django.contrib.auth import get_user_model
 
 
 class Profile(models.Model):
-
 
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_of_birth=models.DateField(blank=True, null=True)
@@ -11,3 +12,25 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'Profile for user {self.user.username}'
+
+
+class Contact(models.Model):
+
+    user_from = models.ForeignKey('auth.User', related_name='rel_from_set', on_delete=models.CASCADE)
+    user_to = models.ForeignKey('auth.User', related_name='rel_to_set', on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    class Meta:
+        ordering = ('-created',)
+    
+    def __str__(self):
+        return f'{self.user_from} follow {self.user_to}'
+
+
+user_model = get_user_model()
+user_model.add_to_class('following', models.ManyToManyField('self', 
+                                                             through=Contact,
+                                                             related_name='followers',
+                                                             symmetrical=False))
+
+
--- images/views.py
+++ images/views.py
@@ -62,7 +62,7 @@ def image_like(request):
 @login_required
 def image_list(request):
     images = Image.objects.all()
-    paginator = Paginator(images, 8)
+    paginator = Paginator(images, 20)
     page = request.GET.get('page')
 
     try:
