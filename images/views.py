a05a52a69ca44b712a0f8dfa4dfa5f347f047140
jeffersonximeness <jeffersonximenes1@gmail.com>
2021-10-05 22:12:27 -0300

    Instanciando redis e criando sistema de contador para ranking de postagens

--- images/views.py
+++ images/views.py
@@ -6,11 +6,17 @@ from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from django.http import HttpResponse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.conf import settings
+
+import redis
 
 from bookmarks.common.decorators import ajax_required
 from actions.utils import create_action
 from .forms import ImageCreateForm
 from .models import Image
+
+
+r = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB)
 
 
 @login_required
@@ -35,8 +41,11 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    total_views = r.incr(f'image:{image.id}:views')
 
-    return render(request, 'images/image/detail.html', {'section': 'images', 'image': image})
+    r.zincrby('image_ranking', 1, image.id)
+
+    return render(request, 'images/image/detail.html', {'section': 'images', 'image': image, 'total_views': total_views})
 
 @ajax_required
 @login_required
@@ -83,3 +92,13 @@ def image_list(request):
         return render(request, 'images/image/list_ajax.html', {'section': 'images', 'images': images})
 
     return render(request, 'images/image/list.html', {'section': 'images', 'images': images})
+
+@login_required
+def image_ranking(request):
+    image_ranking = r.zrange('image_ranking', 0, -1, desc=True)[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+
+    most_viewed = list(Image.objects.filter(id__in=image_ranking_ids))
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+
+    return render(request, 'images/image/ranking.html', {'section': 'images', 'most_viewed': most_viewed})
